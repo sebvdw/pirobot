8dc1d352ace5ccd50366789ffbab014da17872b3
sebvdw <sebastianvandw@gmail.com>
2023-05-31 16:28:04 +0200

    init

---
+++ robot.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(25, GPIO.HIGH)  # Set pin 25 to high (3.3V)
+        
+        # Wait for some time
+        # You can add your code here or use time.sleep() for a delay
+        
+        GPIO.output(25, GPIO.LOW)  # Set pin 25 to low (0V)
+        
+        # Wait for some time
+        # You can add your code here or use time.sleep() for a delay
+
+except KeyboardInterrupt:
+    print("Terminating the script.")
+    GPIO.cleanup()
