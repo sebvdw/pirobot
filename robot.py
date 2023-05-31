2dce0f9eb765ef8ad8ad8f899b6d0cc82bb1a19c
sebvdw <sebastianvandw@gmail.com>
2023-05-31 18:10:33 +0200

    added en

--- robot.py
+++ robot.py
@@ -4,11 +4,12 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
-en = 18
+en1 = 18
 in1 = 23
 in2 = 24
 in3 = 25
 in4 = 12
+en2 = 18
 temp1=1
 
 GPIO.setmode(GPIO.BCM)
@@ -16,14 +17,17 @@ GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(in3,GPIO.OUT)
 GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
+GPIO.setup(en1,GPIO.OUT)
+GPIO.setup(en2,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
 GPIO.output(in3,GPIO.LOW)
 GPIO.output(in4,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+p1=GPIO.PWM(en1,1000)
+p2=GPIO.PWM(en2,1000)
 
-p.start(18)
+p1.start(en1)
+p2.start(en2)
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
@@ -79,17 +83,20 @@ while(1):
 
     elif x=='l':
         print("low")
-        p.ChangeDutyCycle(25)
+        p1.ChangeDutyCycle(25)
+        p2.ChangeDutyCycle(25)
         x='z'
 
     elif x=='m':
         print("medium")
-        p.ChangeDutyCycle(50)
+        p1.ChangeDutyCycle(50)
+        p2.ChangeDutyCycle(50)
         x='z'
 
     elif x=='h':
         print("high")
-        p.ChangeDutyCycle(75)
+        p1.ChangeDutyCycle(75)
+        p2.ChangeDutyCycle(75)
         x='z'
      
     
