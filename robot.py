46c922b122e62e74997aac5b2e2f2d9d94b47194
sebvdw <sebastianvandw@gmail.com>
2023-05-31 16:38:54 +0200

    pin change

--- robot.py
+++ robot.py
@@ -2,16 +2,16 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(25, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(25, GPIO.HIGH)  # Set pin 25 to high (3.3V)
+        GPIO.output(24, GPIO.HIGH)  # Set pin 25 to high (3.3V)
         
         # Wait for some time
         # You can add your code here or use time.sleep() for a delay
         
-        GPIO.output(25, GPIO.LOW)  # Set pin 25 to low (0V)
+        GPIO.output(24, GPIO.LOW)  # Set pin 25 to low (0V)
         
         # Wait for some time
         # You can add your code here or use time.sleep() for a delay
