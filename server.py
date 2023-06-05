03b38cb9cbedf9d9b311969eca855417701a4671
sebvdw <sebastianvandw@gmail.com>
2023-06-05 14:16:01 +0200

    asd

--- server.py
+++ server.py
@@ -20,3 +20,22 @@ def run_server():
     # Set up the server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_host = 'localhost'  # Change this to your desired host
+    server_port = 12345  # Change this to your desired port
+
+    # Bind the server socket to a specific address and port
+    server_socket.bind((server_host, server_port))
+
+    # Start listening for incoming connections
+    server_socket.listen(1)
+    print("Server listening on {}:{}".format(server_host, server_port))
+
+    while True:
+        # Accept a client connection
+        client_socket, client_address = server_socket.accept()
+        print("Client connected:", client_address)
+
+        # Handle the client connection in a separate thread or process
+        handle_client(client_socket)
+
+if __name__ == '__main__':
+    run_server()
