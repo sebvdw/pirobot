9f176891fee3a3d7fb0b2db09c00825f726851ab
sebvdw <sebastianvandw@gmail.com>
2023-06-06 16:01:25 +0200

    this is a test

--- client.py
+++ client.py
@@ -1,23 +1,34 @@
 import socket
 
-def run_client():
-    # Set up the client socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_host = 'localhost'  # Change this to your server's host
-    server_port = 12345  # Change this to your server's port
+# Server information
+server_address = 'localhost'  # Replace with the server IP address
+server_port = 8000  # Replace with the server port number
 
+# Create a TCP/IP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
     # Connect to the server
-    client_socket.connect((server_host, server_port))
-    print("Connected to {}:{}".format(server_host, server_port))
+    client_socket.connect((server_address, server_port))
+    print('Connected to the server.')
 
-    # Send and receive messages with the server
     while True:
-        # Receive the server's response
-        response = client_socket.recv(1024).decode()
-        print("Received response:", response)
-
+        # Receive data from the server
+        data = client_socket.recv(1024).decode()
+        
+        if not data:
+            # No more data received, connection closed by the server
+            print('Server disconnected.')
+            break
+        
+        # Print the received command
+        print('Received command:', data)
+        
+        # Process the command here (add your own code)
+        # ...
+        
+except ConnectionRefusedError:
+    print('Connection refused. Make sure the server is running.')
+finally:
     # Close the client socket
     client_socket.close()
-
-if __name__ == '__main__':
-    run_client()
--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 import socket
 import threading
+import tkinter as tk
 
-def handle_client(client_socket):
+# Define client_socket as a global variable
+client_socket = None
+
+def handle_client():
+    global client_socket  # Access the global client_socket variable
+
     while True:
         try:
             data = input("Enter data to send: ")
@@ -12,9 +18,33 @@ def handle_client(client_socket):
 
     client_socket.close()
 
+def send_command(command):
+    global client_socket  # Access the global client_socket variable
+    client_socket.send(command.encode())
+
+def create_window():
+    window = tk.Tk()
+    window.title("Command Window")
+
+    forward_button = tk.Button(window, text="Forward", command=lambda: send_command("forward"))
+    forward_button.pack()
+
+    backward_button = tk.Button(window, text="Backward", command=lambda: send_command("backward"))
+    backward_button.pack()
+
+    right_button = tk.Button(window, text="Right", command=lambda: send_command("right"))
+    right_button.pack()
+
+    left_button = tk.Button(window, text="Left", command=lambda: send_command("left"))
+    left_button.pack()
+
+    window.mainloop()
+
 def start_server():
+    global client_socket  # Access the global client_socket variable
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_host = '192.168.45.1'
+    server_host = 'localhost'
     server_port = 8000
     server_socket.bind((server_host, server_port))
     server_socket.listen(1)
@@ -23,7 +53,10 @@ def start_server():
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Connection established with {client_address[0]}:{client_address[1]}")
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread = threading.Thread(target=handle_client)
         client_thread.start()
+
+        # Create the tkinter window in the main thread (no need for a separate thread)
+        create_window()
 
 start_server()
